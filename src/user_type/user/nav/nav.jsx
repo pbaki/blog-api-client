0b4a29ff5e51c9c84678ffdc00cc9f57574481df
pbaki <pbaki999@gmail.com>
2024-01-29 23:55:55 +0100

    Initial skeleton

--- src/user_type/user/nav/nav.jsx
+++ src/user_type/user/nav/nav.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import "./nav.css";
 
 function Nav() {
-  return <>Navigation</>;
+  return (
+    <div className="Navigation">
+      <div className="navHomepage">Homepage</div>
+      <div className="navPosts">Blog Posts</div>
+      <div className="navLogin">Login</div>
+    </div>
+  );
 }
 
 export default Nav;
